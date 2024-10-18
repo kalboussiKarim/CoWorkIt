b02af1d73981c4918da6c23a6bc9f46e37c34b31
kalboussi karim <kalboussikarim3@gmail.com>
2024-10-18 13:32:11 +0100

    added the view room from  booking page

--- app/bookings/page.jsx
+++ app/bookings/page.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import Heading from "@/components/Heading";
+import getMyBookings from "../actions/getMyBookings";
+import BookedRoomCard from "@/components/BookedRoomCard";
 
-function BookingsPage() {
-  return <div>BookingsPage</div>;
-}
+const BookingsPage = async () => {
+  const bookings = await getMyBookings();
+
+  return (
+    <>
+      {bookings.length === 0 ? (
+        <p className="text-gray-600 mt-4">You have no bookings </p>
+      ) : (
+        bookings.map((booking) => (
+          <BookedRoomCard key={booking.$id} booking={booking} />
+        ))
+      )}
+    </>
+  );
+};
 
 export default BookingsPage;
